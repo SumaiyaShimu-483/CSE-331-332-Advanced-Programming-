22fb33b513df47fb002555eae0e3d00ab7ff6773
Sumaiya Akter Shimu <109482577+SumaiyaShimu-483@users.noreply.github.com>
2023-12-29 09:38:26 +0600

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,13 @@
+
+package assignmentanisulislam;
+public class Assignment1 {
+  public static void main(String[] args){
+      String a =" id: 101";
+      String b ="title: iphone15";
+      String c ="price: 1895euros";
+      String d ="descriptio: perfact product with best image quality";
+      String e ="category: phone";
+      
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e);
+    }   
+}
